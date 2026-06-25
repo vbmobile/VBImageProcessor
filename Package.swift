20334f22efd9dc212d8b65ee5371d5f9c7d66aef
Tiago Silva <tiago.silva@vision-box.com>
2026-06-25 13:23:28 +0100

    TRAVELERID-14915: AMAImageProcessor 2.0.1 — rebuilt against AMAUtils 3.0.2 + restore wrapper

--- Package.swift
+++ Package.swift
@@ -9,13 +9,25 @@ let package = Package(
     products: [
         .library(
             name: "AMAImageProcessor",
-            targets: ["AMAImageProcessor"]),
+            targets: ["AMAImageProcessorWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vbmobile/VBUtils", exact: "3.0.2"),
     ],
     targets: [
         .binaryTarget(
             name: "AMAImageProcessor",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMAImageProcessor/AMAImageProcessor-2.0.0.zip",
-            checksum: "1358301244bd23270c83f32ce3de0a286fea4cef71ed4d69745cdbf32779de6b"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMAImageProcessor/AMAImageProcessor-2.0.1.zip",
+            checksum: "8e80ffef7c332cc3206ee9637455fe9b99cfd7d2be48dbc5c26a78b046e980b7"
+        ),
+        .target(
+            name: "AMAImageProcessorWrapper",
+            dependencies: [
+                .target(name: "AMAImageProcessor"),
+                .product(name: "AMAUtils", package: "VBUtils")
+            ],
+            path: "Sources",
+            sources: ["dummy.swift"]
         )
     ],
     swiftLanguageVersions: [.v5]
